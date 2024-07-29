d5103cb5c58a9307b517b7c6154ab1ccad45ac80
vladver <vladver80@mail.ru>
2024-07-29 20:52:08 +0300

    Add MudBlazor 7.4.0 to NBCEpagBret.Manager

--- Presentation/NBCEpagBret.Manager/Program.cs
+++ Presentation/NBCEpagBret.Manager/Program.cs
@@ -1,10 +1,12 @@
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
+using MudBlazor.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
+builder.Services.AddMudServices();
 
 var app = builder.Build();
 
